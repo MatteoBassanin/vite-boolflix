3e9bc1cac8baa13d31d5c80e66e588ca74d543c4
MatteoBassanin <matteo.bassanin@gmail.com>
2023-03-31 15:22:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <MyHeader></MyHeader>
+</template>
+
+<script>
+import axios from 'axios';
+import MyHeader from './components/MyHeader.vue';
+import { store } from './store.js';
+
+export default {
+  components: {
+    MyHeader,
+  },
+
+  data() {
+    return {
+      store
+    }
+  }
+}
+</script>
+
+
+<style></style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>ciao</h1>
+</template>
+
+<script>
+
+export default {
+    name: 'MyHeader'
+}
+</script>
+
+<style scoped></style>
