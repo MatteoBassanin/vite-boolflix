a21f2ccf2b3be0342d81206a6763ec616bd18a37
MatteoBassanin <matteo.bassanin@gmail.com>
2023-04-05 16:32:18 +0200

    fixed tvseries graphic

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -8,7 +8,7 @@
             </MyCard>
         </ul>
         <h1 v-if="store.arrayTvSeries != ''">Serie Tv trovate</h1>
-        <ul class="d_flex flex_wrap ">
+        <ul class="d_flex flex_wrap justify_between">
             <MyCardTv v-for="(card, index) in store.arrayTvSeries" :title="card.name" :title_original="card.original_name"
                 :description="card.overview" :language="card.original_language" :rate="card.vote_average"
                 :poster="`${store.apiImage}${card.poster_path}`" :key="index"></MyCardTv>
